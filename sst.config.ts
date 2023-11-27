ddf0f6d7a166154c0a487ab7d949c9e9f3aec7b0
Tony Samsom <tsamsom@shaw.ca>
2023-11-27 07:40:04 -0700

    react part 1

--- sst.config.ts
+++ sst.config.ts
@@ -3,6 +3,7 @@ import { SSTConfig } from "sst";
 import { StorageStack } from "./stacks/StorageStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { FrontendStack } from "./stacks/FrontendStack";
 
 export default {
   config(_input) {
@@ -12,6 +13,10 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
+    app
+      .stack(StorageStack)
+      .stack(ApiStack)
+      .stack(AuthStack)
+      .stack(FrontendStack);
   },
 } satisfies SSTConfig;
